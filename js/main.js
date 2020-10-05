2738dcae99d263cccb80a0eaed11103700100f1a
Neil Jhang <a3817001@gmail.com>
2020-10-05 19:59:33 +0800

    Update to new API

--- js/main.js
+++ js/main.js
@@ -3,28 +3,29 @@ function randomQuote() {
       cache: false
     });
     $.getJSON(
-      "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+      "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand",
       function(q) {
-        $("#text").html(q[0].content);
-        $("#author").text(q[0].title);
+        $("#text").html(q[0].content.rendered);
+        $("#author").text(q[0].title.rendered);
         $("#tweet-quote").attr(
           "href",
           "https://twitter.com/intent/tweet?text=" +
-          q[0].content.replace(/(<([^>]+)>)/gi, "") +
+          q[0].content.rendered.replace(/(<([^>]+)>)/gi, "") +
           " - " +
-          q[0].title
+          q[0].title.rendered
         );
         $("#fb-quote").attr(
           "href",
           "https://www.facebook.com/sharer/sharer.php?u=" +
           window.location.href + "&quote=" +
-          q[0].content.replace(/(<([^>]+)>)/gi, "") +
+          q[0].content.rendered.replace(/(<([^>]+)>)/gi, "") +
           " - " +
-          q[0].title
+          q[0].title.rendered
         );
       }
     );
   }
+
   $(document).ready(function() {
     randomQuote();
     $("#new-quote").click(function() {
